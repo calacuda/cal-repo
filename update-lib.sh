bbb75161bbe2c3b084fdb63f76ef6ef856cc0636
Eoghan West <eowest@gmail.com>
2023-08-20 20:01:22 -0400

    removed hyphens from function names

--- update-lib.sh
+++ update-lib.sh
@@ -7,13 +7,13 @@ build_pkg () {
   cd -
 }
 
-push-changes () {
+push_changes () {
   git add x86_64
   git commit -m "scheduled db update"
   git push
 }
 
-add-to-repo () {
+add_to_repo () {
   cd x86_64
   repo-add cal-repo.db.tar.gz *.pkg.tar.zst
   mv cal-repo.db.tar.gz cal-repo.db
@@ -21,20 +21,20 @@ add-to-repo () {
   cd -
 }
 
-build-pkgs () {
+build_pkgs () {
   for dir in pkgbuilds/x86_64/*; do 
     build_pkg $dir
   done
 }
 
-update-one () {
+update_one () {
   build_pkg $1
-  add-to-repo
-  push-changes
+  add_to_repo
+  push_changes
 }
 
-update-all () {
-  build-pkgs
-  add-to-repo
-  push-changes
+update_all () {
+  build_pkgs
+  add_to_repo
+  push_changes
 }
